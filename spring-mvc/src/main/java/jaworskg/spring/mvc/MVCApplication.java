67e4ac2703fb19570c4fd1ad65ff1d701d5f1ba9
Grzegorz Jaworski <Grzegorz_Jaworski@epam.com>
2017-10-07 11:25:04 +0200

    Added simple MVC application

---
+++ spring-mvc/src/main/java/jaworskg/spring/mvc/MVCApplication.java
@@ -0,0 +1,13 @@
+package jaworskg.spring.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MVCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MVCApplication.class, args);
+    }
+
+}
