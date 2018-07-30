051c61e7d66d78db65f35cf8bc06d554c0384e54
Grzegorz Jaworski <Grzegorz_Jaworski@epam.com>
2018-07-30 21:15:50 +0200

    JDBC: Added sample JDBC module

---
+++ spring-jdbc/src/main/java/jaworskg/spring/jdbc/runners/DatabaseRunner.java
@@ -0,0 +1,49 @@
+package jaworskg.spring.jdbc.runners;
+
+import jaworskg.spring.jdbc.model.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Component
+public class DatabaseRunner implements CommandLineRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseRunner.class);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    DatabaseRunner(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        LOGGER.info("Creating tables");
+
+        jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE customers(id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+        // Split up the array of whole names into an array of first/last names
+        List<Object[]> splitUpNames = Stream.of("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long")
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        splitUpNames.forEach(name -> LOGGER.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+
+        LOGGER.info("Querying for customer records where first_name = 'Josh':");
+        jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[]{"Josh"},
+                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
+        ).forEach(customer -> LOGGER.info(customer.toString()));
+    }
+
+}
